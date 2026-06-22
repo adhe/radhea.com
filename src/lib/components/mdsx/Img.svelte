80a479dd183565dcc5d9f75bfcc1855e9201bdf9
adhe <adhepluk@gmail.com>
2026-06-22 16:36:39 +0700

    Add thin border to img

--- src/lib/components/mdsx/Img.svelte
+++ src/lib/components/mdsx/Img.svelte
@@ -2,4 +2,8 @@
   let { src, alt }: { src?: string; alt?: string } = $props();
 </script>
 
-<img {src} {alt} class="mb-4 tablet:mb-6 rounded-lg tablet:rounded-xl" />
+<img
+  {src}
+  {alt}
+  class="mb-4 tablet:mb-6 rounded-lg tablet:rounded-xl border border-gray-400/10"
+/>
